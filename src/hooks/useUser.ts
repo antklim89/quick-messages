9b272ead66636ccd334213e6aa5e489ed0cc282c
Anton <antklim89@mail.ru>
2022-08-16 22:15:48 +0300

    feat: add uid field to IUser type

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,5 +1,5 @@
 import { onAuthStateChanged } from 'firebase/auth';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { auth } from '~/firebase/app';
 import { IUser } from '~/types';
 
@@ -7,17 +7,20 @@ import { IUser } from '~/types';
 export function useUser() {
     const [user, setUser] = useState<IUser|null>(null);
 
-    onAuthStateChanged(auth, (currentUser) => {
-        if (currentUser) {
-            const { email, displayName } = currentUser;
-            setUser({
-                email,
-                name: displayName || 'anonim',
-            });
-        } else {
-            setUser(null);
-        }
-    });
+    useEffect(() => {
+        onAuthStateChanged(auth, (currentUser) => {
+            if (currentUser) {
+                const { email, displayName, uid } = currentUser;
+                setUser({
+                    email,
+                    name: displayName || 'anonim',
+                    uid,
+                });
+            } else {
+                setUser(null);
+            }
+        });
+    }, []);
 
     return user;
 }
--- src/types/user.ts
+++ src/types/user.ts
@@ -2,4 +2,5 @@
 export interface IUser {
     email: string|null
     name: string
+    uid: string
 }
