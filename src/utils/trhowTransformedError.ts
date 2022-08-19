17586db0ee3b2507285f6939bb4a86e1c767e9de
Anton <antklim89@mail.ru>
2022-08-19 21:30:19 +0300

    feat: added zod errors

--- src/utils/trhowTransformedError.ts
+++ src/utils/trhowTransformedError.ts
@@ -1,10 +1,17 @@
-import { AuthError } from 'firebase/auth';
+import type { FirebaseError } from 'firebase/app';
+import { ZodError } from 'zod';
 
+
+const isFirebaseError = (error: unknown): error is FirebaseError => error instanceof Error && error.name === 'FirebaseError';
 
 export function trhowTransformedError(error: unknown): never {
-    if ((error as AuthError).code) {
-        const errorMessage = (error as AuthError).code.replace('/', ' error: ').replace(/-/g, ' ');
+    if (error instanceof ZodError) {
+        throw new Error(error.message);
+    }
+    if (isFirebaseError(error)) {
+        const errorMessage = error.code.replace('/', ' error: ').replace(/-/g, ' ');
         throw new Error(errorMessage);
     }
+    console.error(error);
     throw new Error('Unexpected error. Try again later.');
 }
