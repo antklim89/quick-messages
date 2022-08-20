999d5550d104699eb156a9455a6e2bc1ebd1f241
Anton <antklim89@mail.ru>
2022-08-20 12:43:15 +0300

    refactor: remove timestamp transformation

--- src/utils/transformFirebaseResponse.ts
+++ src/utils/transformFirebaseResponse.ts
@@ -1,18 +1,9 @@
 import { DocumentData, QueryDocumentSnapshot } from 'firebase/firestore/lite';
-import forEach from 'lodash/forEach';
-import has from 'lodash/has';
-import set from 'lodash/set';
 
 
 export function transformFirebaseResponse(messageDoc: QueryDocumentSnapshot<DocumentData>): unknown {
-    const result = {
+    return {
         id: messageDoc.id,
         ...messageDoc.data(),
     };
-
-    forEach(result, (value: unknown, key) => {
-        if (has(value, 'seconds')) set(result, key, new Date((value as { seconds: number; }).seconds * 1000).toLocaleString());
-    });
-
-    return result;
 }
