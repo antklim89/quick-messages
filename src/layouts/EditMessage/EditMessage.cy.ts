aa465ffbea424e4c053b83a428b84c0002f47d54
Anton <antklim89@mail.ru>
2022-08-21 14:41:47 +0300

    test: update tests

--- src/layouts/EditMessage/EditMessage.cy.ts
+++ src/layouts/EditMessage/EditMessage.cy.ts
@@ -1,9 +1,13 @@
 import { faker } from '@faker-js/faker';
 import { createUserWithEmailAndPassword, signOut } from 'firebase/auth';
-import { auth } from '~/firebase/app';
+import { collection, getDocs, query, where } from 'firebase/firestore/lite';
+import { auth, Collection, db } from '~/firebase/app';
 
 
 describe('EditMessage', () => {
+    const bodyInput = faker.lorem.words(20).slice(0, 200);
+    const invalidBodyInput = 'X';
+
     before(() => {
         signOut(auth);
         cy.visit('/create-message');
@@ -19,15 +23,27 @@ describe('EditMessage', () => {
         cy.getTestId('message-body-input').should('exist');
     });
 
-    it('should create new message', () => {
-        cy.getTestId('message-body-input').type('X');
+    it('should not create new message', () => {
+        cy.getTestId('message-body-input').type(invalidBodyInput);
         cy.getTestId('submit-message-button').should('be.disabled');
     });
 
     it('should create new message', () => {
-        cy.getTestId('message-body-input').type('Lorem ipsum');
+        cy.getTestId('message-body-input').clear().type(bodyInput);
         cy.getTestId('submit-message-button').click();
         cy.contains(/message successfully posted/i).should('exist');
         cy.getTestId('message-body-input').should('be.empty');
+
+
+    });
+
+    it('new message should be in database', async () => {
+        const updatedSnap = await getDocs(query(collection(db, Collection.MESSAGES), where('body', '==', bodyInput)));
+        expect(updatedSnap.docs).to.have.length(1);
+    });
+
+    it('invalid message should not be in database', async () => {
+        const updatedSnap = await getDocs(query(collection(db, Collection.MESSAGES), where('body', '==', invalidBodyInput)));
+        expect(updatedSnap.docs).to.have.length(0);
     });
 });
