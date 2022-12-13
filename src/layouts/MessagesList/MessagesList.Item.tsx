69772239bf2a745c30cf8e6c4a9fb58170840536
Anton <antklim89@mail.ru>
2022-12-13 21:37:05 +0300

    update MessagesListItem

--- src/layouts/MessagesList/MessagesList.Item.tsx
+++ src/layouts/MessagesList/MessagesList.Item.tsx
@@ -1,23 +1,46 @@
-import { Heading, Flex, Text } from '@chakra-ui/react';
+import { Heading, Flex, Text, IconButton } from '@chakra-ui/react';
 import { FC } from 'react';
+import { FaBookmark, FaHeart, FaReply } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 import { MessagesListItemProps } from './MessagesList.types';
 
 
 const MessagesListItem: FC<MessagesListItemProps> = ({ body, author, createdAt }) => {
     return (
-        <Flex boxShadow="lg" flexDirection="column" p={4}>
-            <Heading as="h5" fontSize="md">
-                <Text>{author.name} - {new Date(createdAt).toLocaleString()}</Text>
-            </Heading>
-            <Text
-                bg="gray.50"
-                border="1px solid rgba(0,0,0,0.2)"
-                borderRadius="md"
-                my={4}
+        <Flex
+            border="1px"
+            borderColor="primary.600"
+            borderRadius="lg"
+            boxShadow="md"
+            flexDirection="column"
+            m={4}
+        >
+            <Heading
+                as="h5"
+                display="flex"
+                fontSize="md"
+                justifyContent="space-between"
                 p={4}
             >
+                <Link to={`/user/${author.id}`}>
+                    <Text fontSize="xl">{author.name}</Text>
+                </Link>
+                <Text>{new Date(createdAt).toLocaleString()}</Text>
+            </Heading>
+            <Text my={4} p={4}>
                 {body}
             </Text>
+            <Flex>
+                <IconButton aria-label="reply" flex="1 1 100%" variant="ghost">
+                    <FaReply />
+                </IconButton>
+                <IconButton aria-label="like" flex="1 1 100%" variant="ghost">
+                    <FaHeart />
+                </IconButton>
+                <IconButton aria-label="bookmark" flex="1 1 100%" variant="ghost">
+                    <FaBookmark />
+                </IconButton>
+            </Flex>
         </Flex>
     );
 };
