9af1d3be81ee08cac5b0bf9fea50148958a47767
Anton <antklim89@mail.ru>
2023-01-06 22:13:13 +0300

    like and unlike message

--- src/layouts/Message/MessageLikeButton.tsx
+++ src/layouts/Message/MessageLikeButton.tsx
@@ -1,16 +1,16 @@
-import { IconButton } from '@chakra-ui/react';
+import { IconButton, Text } from '@chakra-ui/react';
 import { FC, useCallback } from 'react';
 import { FaHeart } from 'react-icons/fa';
 import { useLikeRequest } from '~/requests/likeRequest';
 import { IMessage } from '~/types';
 
 
-const MessageLikeButton: FC<IMessage> = ({ id: messageId, hasLiked }) => {
+const MessageLikeButton: FC<IMessage> = ({ id: messageId, hasLiked, likesCount }) => {
     const { isLoading, mutate } = useLikeRequest({ message: messageId });
 
     const handleLike = useCallback(() => {
-        mutate();
-    }, []);
+        mutate({ hasLiked });
+    }, [hasLiked]);
 
     return (
         <IconButton
@@ -18,11 +18,14 @@ const MessageLikeButton: FC<IMessage> = ({ id: messageId, hasLiked }) => {
             color={hasLiked ? 'red.600' : 'primary.600'}
             data-cy="message-like-button"
             flex="1 1 100%"
-            isLoading={isLoading}
+            isDisabled={isLoading}
             variant="ghost"
             onClick={handleLike}
         >
-            <FaHeart />
+            <>
+                <FaHeart />
+                <Text color="primary.600" fontSize="xl" mx={1}>{likesCount}</Text>
+            </>
         </IconButton>
     );
 };
--- src/requests/likeRequest.ts
+++ src/requests/likeRequest.ts
@@ -1,7 +1,9 @@
 import { useToast } from '@chakra-ui/react';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { QueryName } from './constants';
 import { useUser } from './useUser';
 import supabase from '~/supabase/app';
+import { IMessage } from '~/types';
 
 
 export function useLikeRequest({ message }: {message: number}) {
@@ -9,18 +11,35 @@ export function useLikeRequest({ message }: {message: number}) {
     const toast = useToast();
     const queryClient = useQueryClient();
 
-    return useMutation<void, Error, void>({
-        async mutationFn() {
-            if (!userId) throw new Error('You are not authenticated');
+    return useMutation<void, Error, {hasLiked: boolean}>({
+        async mutationFn({ hasLiked }) {
+            if (!userId) throw new Error('Login to like messages');
 
-            const { error } = await supabase
-                .from('likes')
-                .insert({ message, user: userId });
-            if (error) throw error;
+            if (hasLiked) {
+                const { error } = await supabase
+                    .from('likes')
+                    .delete()
+                    .eq('message', message)
+                    .eq('user', userId);
+                if (error) throw new Error('Like fail. Try again later.');
+            } else {
+                const { error } = await supabase
+                    .from('likes')
+                    .insert({ message, user: userId });
+                if (error) throw new Error('Unlike fail. Try again later.');
+            }
         },
-        onSuccess() {
-            toast({ title: 'You have successfully logged in!', status: 'success' });
-            queryClient.invalidateQueries();
+        onSuccess(_, { hasLiked }) {
+            queryClient.setQueryData<IMessage>(
+                [QueryName.FIND_MESSAGE, message],
+                (oldMessage) => (oldMessage
+                    ? ({
+                        ...oldMessage,
+                        hasLiked: !hasLiked,
+                        likesCount: hasLiked ? oldMessage.likesCount - 1 : oldMessage.likesCount + 1,
+                    })
+                    : undefined),
+            );
         },
         onError(error) {
             toast({ title: error.message, status: 'error' });
