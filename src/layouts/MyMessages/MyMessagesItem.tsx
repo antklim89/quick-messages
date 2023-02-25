983072bac946edb7dabdcc6982ecc22e3cd945cf
Anton <antklim89@mail.ru>
2023-02-25 20:11:11 +0300

    update MyMessagesItem

--- src/layouts/MyMessages/MyMessagesItem.tsx
+++ src/layouts/MyMessages/MyMessagesItem.tsx
@@ -5,65 +5,76 @@ import { FC, useCallback } from 'react';
 import { FaBookmark, FaHeart, FaTrash } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import ConfirmDialog from '~/components/ConfirmDialog';
-import { useUser, useDeleteMessageRequest } from '~/requests-hooks';
+import { useUser, useDeleteMessageRequest, useLikeRequest } from '~/requests-hooks';
 import { IMessage } from '~/types';
 
 
-const MyMessagesItem: FC<IMessage> = ({
-    id, body, likesCount, favoritesCount, createdAt, author,
-}) => {
+const MyMessagesItem: FC<IMessage> = (message) => {
+    const {
+        id, body, favoritesCount, createdAt, author,
+    } = message;
+    const { data: { likesCount } } = useLikeRequest(message);
     const { id: userId } = useUser();
     const { mutate, isLoading } = useDeleteMessageRequest({ messageId: id });
 
-    const handleDeleteMessage = useCallback(() => mutate(), [mutate]);
+    const handleDeleteMessage = useCallback(() => mutate(), []);
 
     return (
-        <Box>
-            <Text as={Link} fontSize="sm" to={`/message/${id}`}>
-                {new Date(createdAt).toLocaleString()}
-                {author.id === userId ? null : `by ${author.name}`}
-            </Text>
-            <HStack gap={6} p={4}>
-                <Text mr="auto">
-                    {body}
-                </Text>
-                <ConfirmDialog
-                    isLoading={isLoading}
-                    message="Are you sure you want to delete message!"
-                    onConfirm={handleDeleteMessage}
+        <>
+            <HStack>
+                <VStack
+                    alignItems="flex-start"
+                    as={Link}
+                    to={`/message/${id}`}
+                    width="100%"
                 >
-                    {(toggle) => (
-                        <IconButton
-                            aria-label="delete message"
-                            colorScheme="red"
-                            isLoading={isLoading}
-                            variant="ghost"
-                            onClick={toggle}
-                        >
-                            <FaTrash />
-                        </IconButton>
-                    )}
-                </ConfirmDialog>
-
-                <VStack>
-                    <Box
-                        alignItems="center"
-                        color="red"
-                        display="flex"
-                    >
-                        <FaHeart />&nbsp;{likesCount}
-                    </Box>
-                    <Box
-                        alignItems="center"
-                        color="orange"
-                        display="flex"
-                    >
-                        <FaBookmark />&nbsp;{favoritesCount}
-                    </Box>
+                    <Text fontSize="sm">
+                        {new Date(createdAt).toLocaleString()}
+                        {author.id === userId ? null : `by ${author.name}`}
+                    </Text>
+                    <Text mr="auto">
+                        {body}
+                    </Text>
                 </VStack>
+                <HStack gap={6} p={4}>
+                    <ConfirmDialog
+                        isLoading={isLoading}
+                        message="Are you sure you want to delete message!"
+                        onConfirm={handleDeleteMessage}
+                    >
+                        {(toggle) => (
+                            <IconButton
+                                aria-label="delete message"
+                                colorScheme="red"
+                                isLoading={isLoading}
+                                variant="ghost"
+                                onClick={toggle}
+                            >
+                                <FaTrash />
+                            </IconButton>
+                        )}
+                    </ConfirmDialog>
+
+                    <VStack>
+                        <Box
+                            alignItems="center"
+                            color="red"
+                            display="flex"
+                        >
+                            <FaHeart />&nbsp;{likesCount}
+                        </Box>
+                        <Box
+                            alignItems="center"
+                            color="orange"
+                            display="flex"
+                        >
+                            <FaBookmark />&nbsp;{favoritesCount}
+                        </Box>
+                    </VStack>
+                </HStack>
             </HStack>
-            <Divider mb={2} />
-        </Box>
+            <Divider />
+        </>
     );
 };
 
