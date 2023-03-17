24a1c9f207e0c9333dd6d13f14c0ec011e1dd178
Anton <antklim89@mail.ru>
2023-03-17 14:39:51 +0300

    update profile

--- src/layouts/Profile/Profile.tsx
+++ src/layouts/Profile/Profile.tsx
@@ -1,4 +1,6 @@
-import { Box, Button, Card, Container } from '@chakra-ui/react';
+import {
+    Box, Button, Card, Container, SimpleGrid,
+} from '@chakra-ui/react';
 import { FC } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 
@@ -9,20 +11,18 @@ const Profile: FC = () => {
             <Box
                 display="flex"
                 flexDirection={['column', 'column', 'row']}
-                gap={4}
+                gap={2}
             >
-                <Box
-                    display="flex"
-                    flexDirection={['row', 'row', 'column']}
-                    gap={4}
-                    minWidth="200px"
+                <SimpleGrid
+                    gap={{ base: 2, md: 0 }}
                     sx={{ '& a.active': { bgColor: 'primary.800' } }}
+                    templateColumns={{ base: 'repeat(2, 1fr)', md: '1fr' }}
                 >
                     <Button as={NavLink} to="info" >Info</Button>
-                    <Button as={NavLink} to="reset-password" >Reset Password</Button>
-                    <Button as={NavLink} to="my-messages" >My Messages</Button>
-                    <Button as={NavLink} to="my-favorites" >My Favorites</Button>
-                </Box>
+                    <Button as={NavLink} to="reset-password">Reset Password</Button>
+                    <Button as={NavLink} to="my-messages">Messages</Button>
+                    <Button as={NavLink} to="my-favorites">Favorites</Button>
+                </SimpleGrid>
                 <Card p={4} width="100%">
                     <Outlet />
                 </Card>
