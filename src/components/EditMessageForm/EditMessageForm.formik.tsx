026885494378d10065b959b447bc6cccb7a586e5
Anton <antklim89@mail.ru>
2023-03-20 14:17:18 +0300

    fix ininity loop after update

--- src/components/EditMessageForm/EditMessageForm.formik.tsx
+++ src/components/EditMessageForm/EditMessageForm.formik.tsx
@@ -1,4 +1,5 @@
 import { useFormik } from 'formik';
+import { useMemo } from 'react';
 import { EditMessageFormProps } from './EditMessageForm.types';
 import { useCreateMessageRequest, useUpdateMessageRequest } from '~/requests-hooks';
 import { editMessageSchema } from '~/schemas';
@@ -9,17 +10,19 @@ export function useEditMessageFormFormik({ message, id, answerToId }: EditMessag
     const { mutateAsync: createMessage } = useCreateMessageRequest({ answerToId });
     const { mutateAsync: updateMessage } = useUpdateMessageRequest({ answerToId });
 
+    const initialValues = useMemo(() => ({
+        body: message?.body || '',
+    }), []);
+
     const formik = useFormik<IEditMessageInput>({
-        initialValues: {
-            body: message?.body || '',
-        },
+        initialValues,
         async onSubmit(values, { resetForm }) {
             if (id) {
                 await updateMessage({ values, messageId: id });
             } else {
                 await createMessage({ values });
+                resetForm();
             }
-            resetForm();
         },
         async validate(values) {
             const result = await editMessageSchema.safeParseAsync(values);
