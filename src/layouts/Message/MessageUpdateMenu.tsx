0c1e2624248e10dcace528c91fd5d86ac3ab21f3
Anton <antklim89@mail.ru>
2023-03-20 14:36:46 +0300

    add user check

--- src/layouts/Message/MessageUpdateMenu.tsx
+++ src/layouts/Message/MessageUpdateMenu.tsx
@@ -3,16 +3,19 @@ import {
 } from '@chakra-ui/react';
 import { FC, useCallback, useRef } from 'react';
 import EditMessageForm from '~/components/EditMessageForm';
+import { useUser } from '~/requests-hooks';
 import { IMessage } from '~/types';
 
 
 const MessageUpdateMenu: FC<IMessage> = (message) => {
+    const { id } = useUser();
     const { isOpen, onOpen, onClose } = useDisclosure();
     const handleOpen = useCallback(() => setTimeout(onOpen, 1), []);
 
     const initialRef = useRef(null);
     const finalRef = useRef(null);
 
+    if (id !== message.author.id) return null;
     return (
         <>
             <MenuItem onClick={handleOpen}>
@@ -27,19 +30,17 @@ const MessageUpdateMenu: FC<IMessage> = (message) => {
             >
                 <ModalOverlay />
                 <ModalContent>
-                    <ModalHeader>Create your account</ModalHeader>
+                    <ModalHeader>Update Message</ModalHeader>
                     <ModalCloseButton />
 
                     <ModalBody pb={6}>
-                        <EditMessageForm id={message.id} message={message} ref={initialRef} />
+                        <EditMessageForm
+                            id={message.id}
+                            messageBody={message.body}
+                            ref={initialRef}
+                            onSuccess={onClose}
+                        />
                     </ModalBody>
-
-                    {/* <ModalFooter>
-                        <Button colorScheme="blue" mr={3}>
-                            Save
-                        </Button>
-                        <Button onClick={onClose}>Cancel</Button>
-                    </ModalFooter> */}
                 </ModalContent>
             </Modal>
         </>
