f0b7dbd688adac7a1a49c336903f30fffd405672
Anton <antklim89@mail.ru>
2023-03-21 15:11:59 +0300

    update supabase createClient

--- src/supabase/app.ts
+++ src/supabase/app.ts
@@ -1,15 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 import { Database } from './types-generated';
+import { VITE_SUPABASE_KEY, VITE_SUPABASE_URL } from '~/constants';
 
 
-const supabaseUrl: string = import.meta.env.VITE_SUPABASE_URL || (() => {
-    throw new Error('VITE_SUPABASE_URL env is required');
-})();
-
-const supabaseKey: string = import.meta.env.VITE_SUPABASE_KEY || (() => {
-    throw new Error('VITE_SUPABASE_KEY env is required');
-})();
-
-const supabase = createClient<Database>(supabaseUrl, supabaseKey);
-
-export default supabase;
+export default createClient<Database>(VITE_SUPABASE_URL, VITE_SUPABASE_KEY);
