fcf3c579815c7f789b626e8f59ddebf29e4fd58e
Anton <antklim89@mail.ru>
2023-04-02 15:20:53 +0300

    feat: resize by width and height

--- src/utils/resizeImage.ts
+++ src/utils/resizeImage.ts
@@ -10,16 +10,17 @@ export function resizeImage(imageFile: File) {
             const ctx = canvas.getContext('2d');
 
             canvas.width = AVATAR_SIZE;
-            canvas.height = canvas.width;
+            canvas.height = AVATAR_SIZE;
 
-
-            const width = (img.width / img.height) * AVATAR_SIZE;
-            const widthOffset = (width - AVATAR_SIZE) / 2;
-
-            ctx?.drawImage(img, -widthOffset, 0, width, AVATAR_SIZE);
-
-            // const dataurl = canvas.toDataURL(imageFile.type);
-            // console.log('===== \n dataurl', dataurl);
+            if (img.width > img.height) {
+                const width = (img.width / img.height) * AVATAR_SIZE;
+                const widthOffset = (width - AVATAR_SIZE) / 2;
+                ctx?.drawImage(img, -widthOffset, 0, width, AVATAR_SIZE);
+            } else {
+                const height = (img.height / img.width) * AVATAR_SIZE;
+                const heightOffset = (height - AVATAR_SIZE) / 2;
+                ctx?.drawImage(img, 0, -heightOffset, AVATAR_SIZE, height);
+            }
 
             return canvas.toBlob((blob) => {
                 if (!blob) throw reject(new Error('Error while converting image.'));
