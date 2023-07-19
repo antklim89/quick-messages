70ae2348659447be08b8a1b8b89f0281a87794f6
Anton <antklim89@mail.ru>
2023-07-19 11:10:07 +0300

    feat: make DateComponent lazy

--- src/components/DateComponent/DateComponent.tsx
+++ src/components/DateComponent/DateComponent.tsx
@@ -1,13 +1,19 @@
 import { Text } from '@chakra-ui/react';
-import { format } from 'date-fns';
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import { DateComponentProps } from './DateComponent.types';
 
 
-const DateComponent: FC<DateComponentProps> = ({ date, format: dateFormate = 'dd-MMM-yyyy H:mm', ...props }) => {
+const DateComponent: FC<DateComponentProps> = ({ date, format: dateFormat = 'dd-MMM-yyyy H:mm', ...props }) => {
+    const [formatedDate, setFormatedDate] = useState<string | null>(null);
+
+    useEffect(() => {
+        import('date-fns')
+            .then(({ format }) => setFormatedDate(format(new Date(date), dateFormat)));
+    }, []);
+
     return (
         <Text {...props} as="span">
-            {format(new Date(date), dateFormate)}
+            {formatedDate}&nbsp;
         </Text>
     );
 };
