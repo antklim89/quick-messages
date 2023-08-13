022fac64c38a0cd8b87196cb1cf3a279425cc97d
Anton <antklim89@mail.ru>
2023-08-13 16:10:22 +0300

    refactor: remove prisma

--- prisma/migrations/20230413165936_add_report_ondelete_cascade/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "reports" DROP CONSTRAINT "reports_messageId_fkey";
-
--- AddForeignKey
-ALTER TABLE "reports" ADD CONSTRAINT "reports_messageId_fkey" FOREIGN KEY ("messageId") REFERENCES "messages"("id") ON DELETE CASCADE ON UPDATE CASCADE;
