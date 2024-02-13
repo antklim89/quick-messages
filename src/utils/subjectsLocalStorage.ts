8e093b92047fa8379a3100cc95176de1ab2b48e4
Anton <antklim89prog@mail.ru>
2024-02-13 20:21:39 +0300

    chore: add filterUniqueSubjects

--- src/utils/subjectsLocalStorage.ts
+++ src/utils/subjectsLocalStorage.ts
@@ -16,6 +16,15 @@ export function getPreviouslySelectedSubjects(): string[] {
 
 export function addToPreviouslySelectedSubjects(subject: string): void {
     const subjects = getPreviouslySelectedSubjects();
-    const localSubjectsStr = JSON.stringify([...new Set([subject, ...subjects.slice(0, 9)])]);
+    const localSubjectsStr = JSON.stringify(filterUniqueSubjects([subject, ...subjects.slice(0, 9)]));
     localStorage.setItem(LOCAL_STORAGE_SUBJECTS, localSubjectsStr);
+}
+
+function filterUniqueSubjects(subjects: string[]): string[] {
+    const result: string[] = [];
+    for (let i = 0; i < subjects.length; i += 1) {
+        const subject = subjects[i];
+        if (subject && result.indexOf(subject) === -1) result.push(subject);
+    }
+    return result;
 }
